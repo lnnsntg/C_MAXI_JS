aa37e5dd370c27683f8fc57f7e9903e0e2584d8b
lenin <lnnsntg@gmail.com>
2021-09-23 16:43:52 +0200

    Eliminados archivos inneccesarios cambiados algunos nombres de carpetas

--- basics-02-added-scripts-imports/assets/scripts/app.js
+++ basics-02-added-scripts-imports/assets/scripts/app.js
@@ -1,1 +1,10 @@
-alert('This works!');
+const defaultResult = 0;
+currentResult = defaultResult;
+
+const add = (num1, num2) => {
+  currentResult = currentResult + parseInt(userInput.value); 
+  console.log(currentResult);  
+  outputResult(currentResult, "ops")
+ };
+
+addBtn.addEventListener("click", add);
