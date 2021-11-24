ab1ddb38454ae0a6d7278e5d3ad36c4c1df04a3a
Lenin Santiago <lnnsntg@gmail.com>
2021-11-24 11:51:08 +0100

    Actualizando repositorio

---
+++ functions-01-starting-code/app.js
@@ -0,0 +1,14 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+
+const start = function starGame() {
+    console.log('Game is startirg...');
+}
+const funcion1 = ()=>{}
+const otra = (2+2)
+
+
+
+
+startGameBtn.addEventListener('click', start)
+console.dir(otra)
